ce8b337e95658745345876db07792ca4f3e5aace
ARandomPerson69 <ndao8080@gmail.com>
2022-06-25 21:51:09 -0500

    Obstacle??

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,10 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D body;
+    public Animator animator;
 
     private Vector2 movement;
-    private bool facingRight = true;
+
 
     // Update is called once per frame
     void Update()
@@ -19,20 +20,17 @@ public class PlayerMovement : MonoBehaviour
         //Set player input to movement values in Vector2
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
-        
+        //Set the movement of the player based on input
         body.MovePosition(body.position + movement * moveSpeed * Time.fixedDeltaTime);
-        if (movement.x < 0)
-        {
-            
-        }
-    }
-
-    private void flip()
-    {
         
     }
+    
 }
